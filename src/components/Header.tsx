9c4d706c856ac9c517f896fb0204be4051f5332a
Jonathan <jtellkampf@gmail.com>
2023-01-27 20:11:44 +0000

    completed header, created banner

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
-import { SearchIcon } from "@heroicons/react/solid";
+import {
+  SearchIcon,
+  GlobalAltIcon,
+  MenuIcon,
+  UserCircleIcon,
+} from "@heroicons/react/solid";
 
 const Header: React.FC = () => {
   return (
@@ -26,7 +31,14 @@ const Header: React.FC = () => {
         <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
       </div>
 
-      <div></div>
+      <div className="flex items-center space-x-4 justify-end text-gray-500">
+        <p className="hidden md:inline cursor-pointer">Become a host</p>
+        <GlobalAltIcon className="h-6 cursor-pointer" />
+        <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
+          <MenuIcon className="h-6" />
+          <UserCircleIcon className="h-6" />
+        </div>
+      </div>
     </header>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
-import Header from "../components/Header";
 import Head from "next/head";
+
+import Header from "../components/Header";
+import Banner from "../components/Banner";
 
 export default function Home() {
   return (
@@ -12,6 +14,7 @@ export default function Home() {
       </Head>
 
       <Header />
+      <Banner />
     </div>
   );
 }

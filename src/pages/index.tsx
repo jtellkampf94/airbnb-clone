d12adce4a32df2d91b9afe3adba09e59f7c455c2
Jonathan <jtellkampf@gmail.com>
2023-01-28 23:05:38 +0000

    Completed banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,6 +15,12 @@ export default function Home() {
 
       <Header />
       <Banner />
+
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+        </section>
+      </main>
     </div>
   );
 }

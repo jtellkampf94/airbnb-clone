8a07a3e1774a641ff35641553f589d8e284c732c
Jonathan <jtellkampf@gmail.com>
2023-02-01 15:56:40 +0000

    Created medium card

---
+++ src/components/MediumCard.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+interface MediumCardProps {
+  img: string;
+  title: string;
+}
+
+const MediumCard: React.FC<MediumCardProps> = ({ img, title }) => {
+  return (
+    <div className="cursor-pointer hover:scale-105 transform transition duration-300 ease-out">
+      <div className="relative h-80 w-80">
+        <Image src={img} alt={title} layout="fill" className="rounded-xl" />
+      </div>
+      <h3 className="text-2xl mt-3">{title}</h3>
+    </div>
+  );
+};
+
+export default MediumCard;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,8 @@ import Head from "next/head";
 
 import Header from "../components/Header";
 import Banner from "../components/Banner";
-import SmallCard from "@/components/SmallCard";
+import SmallCard from "../components/SmallCard";
+import MediumCard from "../components/MediumCard";
 
 interface ExploreData {
   img: string;
@@ -51,6 +52,12 @@ const Home: React.FC<HomeProps> = ({ exploreData, cardsData }) => {
 
         <section>
           <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
+            {cardsData?.map(({ img, title }) => (
+              <MediumCard key={img} img={img} title={title} />
+            ))}
+          </div>
         </section>
       </main>
     </div>

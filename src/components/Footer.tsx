861e8670ae93a12efb4b5b07c7e41c23083e4919
Jonathan <jtellkampf@gmail.com>
2023-02-03 18:26:57 +0000

    Created footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,28 @@
+const Footer: React.FC = () => {
+  return (
+    <div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>Not real site</p>
+        <p>This is a clone</p>
+        <p>Referrals accepted</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Footer;
